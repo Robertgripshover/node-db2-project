01fae24a32de64085f86d5c4fa347a809ec16661
Robert Gripshover <robertgrips@gmail.com>
2023-03-21 15:29:35 -0400

    added the .get for the cars

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -5,7 +5,7 @@ const db = require('../../data/db-config')
 
 
 const getAll = () => {
-  // DO YOUR MAGIC
+  return db('cars')
 }
 
 const getById = () => {
@@ -14,4 +14,11 @@ const getById = () => {
 
 const create = () => {
   // DO YOUR MAGIC
+}
+
+
+module.exports = {
+  getAll,
+  getById,
+  create,
 }
