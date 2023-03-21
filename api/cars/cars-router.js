20074ba0d0084b3908b961016f3b1237afabe1ab
Robert Gripshover <robertgrips@gmail.com>
2023-03-21 15:44:42 -0400

    m

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,8 +1,16 @@
-// DO YOUR MAGIC
 
 const express = require('express')
 
 const Car = require('./cars-model')
+
+const {    
+
+    checkCarId,
+    checkCarPayload,
+    checkVinNumberValid,
+    checkVinNumberUnique,
+
+} = require('./cars-middleware')
 
 const router = express.Router()
 
@@ -16,14 +24,8 @@ router.get('/', async (req, res, next) => {
     }
 })
 
-router.get('/:id', async (req, res, next) => {
-    try {
-        const car = await Car.getById(req.params.id)
-        res.json(car)
-
-    } catch (err) {
-        next(err)
-    }
+router.get('/:id', checkCarId, async (req, res, next) => {
+    res.json(req.car)
 })
 
 router.post('/', async (req, res, next) => {
